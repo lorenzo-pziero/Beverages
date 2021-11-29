2929525c5b71a753cfdc75584618d301d7e96be8
Lorenzo Peruzzo Ziero <91576172+lorenzo-pziero@users.noreply.github.com>
2021-11-28 22:55:16 -0300

    Create beverages.php
    <<<SELLING BEVERAGES>>>

---
+++ beverages.php
@@ -0,0 +1,44 @@
+<?php
+$sell = "SELLING BEVERAGES...";
+function show() {	
+	global $sell;
+	echo $sell;
+}
+show();
+echo "<br/>";
+$beverage1 = "Red Label -> R$84.00";
+    function show1() {
+    	global $beverage1;
+    	echo $beverage1;
+}
+show1();
+echo "<br/>";
+$beverage2 = "Fernet -> R$94.00";
+	function show2() {
+		global $beverage2;
+		echo $beverage2;
+}
+show2();
+echo "<br/>";
+$beverage3 = "Vodka Absolut -> R$83.00";
+	function show3() {
+		global $beverage3;
+		echo $beverage3;
+}
+show3();
+echo "<br/>";
+$beverage4 = "Jagmeisteir -> R$73.00";
+	function show4() {
+		global $beverage4;
+		echo $beverage4;
+}
+show4();
+echo "<br/>";
+$beverage5 = "Gin Tanqueray -> R$72.00";
+	function show5(){
+		global $beverage5;
+		echo $beverage5;
+}
+show5();
+echo "<br/>";
+?>
