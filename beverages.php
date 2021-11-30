d55c421a5655fd23b4d3ffd41c2f8800a8a9b2d6
Lorenzo Peruzzo Ziero <91576172+lorenzo-pziero@users.noreply.github.com>
2021-11-29 22:59:38 -0300

    Update beverages.php

--- beverages.php
+++ beverages.php
@@ -1,35 +1,35 @@
 <?php
 echo "<strong>SELLING BEVERAGES...</strong>";
 echo "<br/>";
-$beverage1 = "Red Label -> R$84.00";
+$beverage1 = "<strong>Red Label</strong> -> R$84.00";
     function show1() {
     	global $beverage1;
     	echo $beverage1;
 }
 show1();
 echo "<br/>";
-$beverage2 = "Fernet -> R$94.00";
+$beverage2 = "<strong>Fernet</strong> -> R$94.00";
 	function show2() {
 		global $beverage2;
 		echo $beverage2;
 }
 show2();
 echo "<br/>";
-$beverage3 = "Vodka Absolut -> R$83.00";
+$beverage3 = "<strong>Vodka Absolut</strong> -> R$83.00";
 	function show3() {
 		global $beverage3;
 		echo $beverage3;
 }
 show3();
 echo "<br/>";
-$beverage4 = "Jagmeisteir -> R$73.00";
+$beverage4 = "<strong>Jagmeisteir</strong> -> R$73.00";
 	function show4() {
 		global $beverage4;
 		echo $beverage4;
 }
 show4();
 echo "<br/>";
-$beverage5 = "Gin Tanqueray -> R$72.00";
+$beverage5 = "<strong>Gin Tanqueray</strong> -> R$72.00";
 	function show5(){
 		global $beverage5;
 		echo $beverage5;
